588786d7cde1523ec43fd24a3849bf7ec76607dc
Raounak Sharma <raounaqsharma@gmail.com>
2016-06-22 14:09:56 +0530

    QRcode generator

---
+++ QR_code.java
@@ -0,0 +1,31 @@
+package myPro;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import net.glxn.qrgen.QRCode;
+import net.glxn.qrgen.image.ImageType;
+
+public class FirstClass
+{
+	public static void main(String args[]) throws Exception
+	{
+		String details = ".... Ronak has made this ....";
+		
+		ByteArrayOutputStream out = QRCode.from(details).to(ImageType.JPG).stream();
+		
+		File f = new File("C:\\Users\\Raounak Sharma\\Desktop\\Ronaq\\Ronaq's_QRcode.jpg");
+		FileOutputStream fos = new FileOutputStream(f);
+		
+		fos.write(out.toByteArray());
+		fos.flush();
+		
+	}
+}
+	
+   
+   
+   
+  
+
